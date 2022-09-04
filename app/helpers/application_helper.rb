63666a8a8717b8f2b7f597b692dd9cfb76635d1d
vishesh kumar <vishesh.kumar.cs.2018@miet.ac.in>
2022-09-04 08:15:19 +0530

    d

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,13 @@
 module ApplicationHelper
+
+  ###### For HTML validation Use EveryTime's ##########
+
+  ####### Access for Customer's functionality ##########
   def customer_role_only
     current_user.roles.first.user_type == 'Customer'
   end
 
+  ####### Access for Property_Owner's functionality ######### 
   def property_owner_role_only
     current_user.roles.first.user_type == 'Property_Owner'
   end
